aceb2781a5ab6d68d7af6f6649da367b623fae8e
Simut Darius <simutdarius7@gmail.com>
2026-08-07 11:30:43 +0200

    I don't know how much it helps but myea

--- push_swap_utils_stack.c
+++ push_swap_utils_stack.c
@@ -1,5 +1,31 @@
 #include "push_swap.h"
 
+
+int optimizedRotate(t_stack *stack,int index)
+{
+	int i;
+	int j;
+	int found;
+	t_node *tmp;
+
+	i = 0;
+	j = 0;
+	found = 0;
+	tmp = stack->head;
+	while(tmp->next)
+	{
+		if(tmp->index == index)
+			found = 1;
+		if(found)
+			j++;
+		else
+			i++;
+		tmp = tmp->next;
+	}
+	printf("%d %d = %d\n",i,j,i < j);
+	return(i < j);
+}
+
 int isSorted(t_stack *stack)
 {
 	t_node *tmp;
@@ -39,20 +65,24 @@ void simpleSort(t_stack *stack)
 
 void quickSort(t_stack *stack_a,t_stack *stack_b)
 {
-	t_node *tmp;
-	int i =0;
-	tmp = stack_a->head;
-	while(tmp && i < stack_a->size/2)
+	int i;
+
+	i = 0;
+	while(stack_a->size != 3)
 	{
-		if(tmp->index == i)
+		if(stack_a->head->index == i)
 		{
 			pushStack(stack_b,stack_a,'b');
 			i++;
 		}
 		else
 		{
-			rotateStack(stack_a,'a');
-			tmp = stack_a->head;
+			if(optimizedRotate(stack_a,i))
+				while(stack_a->head->index != i)
+					rotateStack(stack_a,'a');
+			else
+				while(stack_a->head->index != i)
+					rRotateStack(stack_a,'a');
 		}
 	}
 	simpleSort(stack_a);
