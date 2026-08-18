a30425719fd22ae0d8dfc87e17010ce816672b6c
Simut Darius <simutdarius7@gmail.com>
2026-08-18 09:32:44 +0200

    Fixed , with a shit patch, problem maybe for stack overflow, not sure. btw im pushing this on plane

--- push_swap_utils_stack.c
+++ push_swap_utils_stack.c
@@ -181,12 +181,12 @@ void findPairA(t_stack *stack_a,t_stack *stack_b)
 				minMoves = currMoves;
 				canInd = tmp->index;
 				targInd = target;
-	//	printf("can=%d targ=%d  currMoves=%d minMoves=%d condition=%d\n",
-    //  canInd,target, currMoves, minMoves, currMoves < minMoves);
+		printf("can=%d targ=%d  currMoves=%d minMoves=%d condition=%d\n",
+      canInd,target, currMoves, minMoves, currMoves < minMoves);
 		}
 		tmp = tmp->next;
 	}
-	//printf("%d pari %d\n",canInd,targInd);
+	printf("%d pari %d\n",canInd,targInd);
 	meetPairA(stack_a,stack_b,canInd,targInd);
 //printf("yo%ld %ld %d\n",target->data,candidate->data,minMoves);
 }
@@ -223,24 +223,42 @@ void meetPairB(t_stack *stack_can, t_stack *stack_tar,int canInd,int tarInd)
 	pushStack(stack_tar,stack_can,'a');
 }
 
+int minIndex(t_stack *stack)
+{
+	t_node *node;
+	int min;
+
+	node = stack->head;
+	min = stack->head->index;
+	while(node->next)
+	{
+		node = node->next;
+		if(node->index < min)
+			min = node->index;
+	}
+return min;
+}
+
+
 int findTargetB(t_stack *stack,int candidate)
 {
 	t_node *tmp;
-	int min;
+	long long int min;
 
-	min = maxIndex(stack);
+	min = maxIndex(stack) + 1;
 	tmp = stack->head;
 	while(tmp)
 	{
-		//printf("%d > %d\n",tmp->index,candidate);
-		if(tmp->index > candidate && tmp->index < min)
+		printf("%d > %d | %d\n",tmp->index,candidate,min);
+		if(tmp->index > candidate && tmp->index <= min)
 			{
-			//printf("is good %d %d\n",tmp->index,candidate);
+			printf("is good %d %d\n",tmp->index,candidate);
 				min = tmp->index;
 			}
 		tmp = tmp->next;
 	}
-
+	if(min == maxIndex(stack) + 1)
+		return minIndex(stack);
 return min;
 }
 
@@ -266,8 +284,8 @@ void findPairB(t_stack *stack_a,t_stack *stack_b)
 				minMoves = currMoves;
 				canInd = tmp->index;
 				targInd = target;
-	//	printf("can=%d targ=%d  currMoves=%d minMoves=%d condition=%d\n",
-    //  canInd,target, currMoves, minMoves, currMoves < minMoves);
+		printf("can=%d targ=%d  currMoves=%d minMoves=%d condition=%d\n",
+      canInd,target, currMoves, minMoves, currMoves < minMoves);
 		}
 		tmp = tmp->next;
 	}
@@ -306,20 +324,20 @@ void turkSort(t_stack *stack_a, t_stack *stack_b)
 
 	while(stack_a->size > 3)
 	{
-		//DEBUG_printStack(stack_a,'a');
-		//DEBUG_printStack(stack_b,'b');
+		DEBUG_printStack(stack_a,'a');
+		DEBUG_printStack(stack_b,'b');
 		findPairA(stack_a,stack_b);
 	}
 	if(isSorted(stack_a) == 1)
 		simpleSort(stack_a);
-	//printf("we go back\n");
+	printf("we go back\n");
 	while(stack_b->size > 0)
 	{
-		//DEBUG_printStack(stack_a,'a');
-		//DEBUG_printStack(stack_b,'b');
+		DEBUG_printStack(stack_a,'a');
+		DEBUG_printStack(stack_b,'b');
 		findPairB(stack_a,stack_b);
 	}
-		//DEBUG_printStack(stack_a,'a');
+		DEBUG_printStack(stack_a,'a');
 	doRotationStack(stack_a,optimizedRotate(stack_a,0),'a');
 		DEBUG_printStack(stack_a,'a');
 
